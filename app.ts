7b06f16dcdb299917ad9e2cbe889c681ef19ddb1
Sirwan <sir1afifi@gmail.com>
2020-06-30 20:32:44 +0430

    Add dev

--- app.ts
+++ app.ts
@@ -1,15 +1,16 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import bodyParser from "body-parser";
 import path from "path";
+
+import * as homeController from "./api/homeController";
 
 const app = express();
 
-app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "pug");
-
-app.get("/", (req: Request, res: Response) => {
-  res.render("home", {
-    username: "SirwanAfifi",
-  });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+app.get("/api", homeController.index);
 
 export default app;
